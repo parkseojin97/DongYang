cc56890c2600cd3daee7580467c10c86e8d6eb0e
Ok Jae Ook <okjaeook98@gmail.com>
2022-05-29 23:52:35 +0900

    모달 이슈 해결 + 불필요한 리소스 제거
    1. 모달 이슈 해결 #1
    - thymeleaf 관련 코드가 있어 실행되지 않았음. 템플릿 완성 후 리터칭 예정
    - thymeleaf 적용시 <html> 태그에 xmlns:th="http://www.thymeleaf.org" 속성 적용해야 렌더링됨
    
    2. css, image, font 등 미적용된 리소스 제거
    - 리소스는 cdn으로, 디자인은 통일되게 합니다

--- src/main/java/com/pmis/controller/ProjectController.java
+++ src/main/java/com/pmis/controller/ProjectController.java
@@ -1,61 +1,53 @@
 package com.pmis.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.pmis.service.CmmnService;
-
 @Controller
 public class ProjectController {
 
-
 	@GetMapping("projects")
 	public String projects(Model model) {
-
 		return "projects";
 	}
-	
+
 	@GetMapping("settings")
 	public String settings(Model model) {
-		
 		return "settings";
 	}
-	
+
 	@GetMapping("project")
 	public String selectProject(Model model) {
-		
-		return  "project";
+		return "project";
 	}
-	
+
 	@GetMapping("meeting")
 	public String meeting(Model model) {
-		
 		return "meeting";
 	}
-	
+
 	@GetMapping("meetingroom")
 	public String meetingRoom(Model model) {
-		
+
 		return "meetingroom";
 	}
-	
+
 	@GetMapping("meetinglog")
 	public String meetingLog(Model model) {
-		
+
 		return "meetinglog";
 	}
-	
+
 	@GetMapping("schedule")
 	public String schedule(Model model) {
-		
+
 		return "schedule";
 	}
-	
+
 	@GetMapping("dashboard")
 	public String dashboard(Model model) {
-		
+
 		return "dashboard";
 	}
 

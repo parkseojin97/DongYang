61f905ddc2f88dfa4896511bdb94a703a77d4622
JaeRyeong Jung <73861946+Rwhisper@users.noreply.github.com>
2022-06-07 21:54:01 +0900

    projectDTO status_id 삭제

--- src/main/java/com/pmis/model/ProjectDTO.java
+++ src/main/java/com/pmis/model/ProjectDTO.java
@@ -12,7 +12,6 @@ public class ProjectDTO {
 	private Date project_create_date;
 	private Date project_final_date;
 	private Date project_final_expect_date;
-	private int status_id;
 	private int privacy_scope;
 
 }

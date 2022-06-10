181d5d30a2bc34d5238596bbe175129273958830
JaeRyeong Jung <73861946+Rwhisper@users.noreply.github.com>
2022-06-11 05:38:04 +0900

    mapper 반환값 수정 및 selectOneProject, boardJoinKanban 메서드 생성

--- src/main/java/com/pmis/mapper/ProjectMapper.java
+++ src/main/java/com/pmis/mapper/ProjectMapper.java
@@ -12,6 +12,7 @@ import com.pmis.model.MeetingDTO;
 import com.pmis.model.MeetingLogChatDTO;
 import com.pmis.model.MeetingLogDTO;
 import com.pmis.model.ProjectBoardDTO;
+import com.pmis.model.ProjectBoardJoinKanban;
 import com.pmis.model.ProjectDTO;
 import com.pmis.model.ProjectJoinDTO;
 import com.pmis.model.ProjectRuleDTO;
@@ -27,30 +28,33 @@ public interface ProjectMapper extends DefaultDBInfo {
 	ArrayList<ProjectDTO> selectProjects(UserDTO user);
 	
 	// 공개범위가 public인 프로젝트 목록 불러오기
-	@Select("SELECT * FROM " + PROJECT + " where privacy_scope=1")
+	@Select("SELECT * FROM " + PROJECT + " where privacy_scope='public'")
 	ArrayList<ProjectDTO> selectPublicProjects();	
 	
 	// 공개범위가 public인 프로젝트 갯수 불러오기
-	@Select("SELECT count(*) FROM " + PROJECT + " where privacy_scope=1")
+	@Select("SELECT count(*) FROM " + PROJECT + " where privacy_scope='public'")
 	int selectPublicProjectCnt();	
 	
 	// 공개범위가 public이고 pagination처리가된 프로젝트 목록 불러오기
-	@Select("SELECT * FROM " + PROJECT + " where privacy_scope=1 order by project_id desc limit ${startIndex}, ${pageSize}")
+	@Select("SELECT * FROM " + PROJECT + " where privacy_scope='public' order by project_id desc limit ${startIndex}, ${pageSize}")
 	ArrayList<ProjectDTO> selectPagingProjects(int startIndex, int pageSize);	
 	
 	// 새로운 프로젝트 생성
 	@Insert("INSERT INTO " + PROJECT + " VALUES( null, #{project_name}, #{project_des}, "
 			+ "now(), null, null, #{privacy_scope} )")
-	void createProject(ProjectDTO project);
-	
+	boolean createProject(ProjectDTO project);
 	
 	// 프로젝트 조회
+	@Select("Select * from " + PROJECT + " where project_id=#{project_id}")
+	ProjectDTO selectOneProject(ProjectDTO project);
+	
+	// 가장 최신의 프로젝트 조회
 	@Select("Select * from " + PROJECT + " order by project_id desc limit 1")
-	ProjectDTO selectProject();
+	ProjectDTO selectLatestProject();
 	
 	// 프로젝트 삭제
 	@Delete("DELETE FROM " + PROJECT + " WHERE project_id=${project_id}")
-	void deleteProject(ProjectDTO project);
+	boolean deleteProject(ProjectDTO project);
 	
 	// task
 	// task 종류(칸반) 불러오기
@@ -59,11 +63,11 @@ public interface ProjectMapper extends DefaultDBInfo {
 	
 	// 새로운 칸반 생성
 	@Insert("INSERT INTO " + PROJECTSTATUS + " VALUES(null, #{project_status}, ${project_id})")
-	void createBoardStatus(ProjectStatusDTO status);
+	boolean createBoardStatus(ProjectStatusDTO status);
 	
 	// 칸반 삭제
 	@Delete("DELETE FROM " + PROJECTBOARD + " WHERE kanban_id=${kanban_id}")
-	void deleteBoardStatus(ProjectStatusDTO status);
+	boolean deleteBoardStatus(ProjectStatusDTO status);
 	
 	// 프로젝트 task(board) 불러오기 
 	@Select("SELECT * FROM " + PROJECTBOARD + " where project_id=${project_id} ORDER BY board_id DESC")
@@ -71,20 +75,27 @@ public interface ProjectMapper extends DefaultDBInfo {
 	
 	// 새로운 task(board) 생성
 	@Insert("INSERT INTO " + PROJECTBOARD
-			+ " VALUES(null,${project_id},#{board_subject},#{board_content},now(),#{create_user_email}, #{start_user_email}, 0"
+			+ " VALUES(null,${project_id},#{board_subject},#{board_content},now(),#{create_user_email}, #{start_user_email}, 0,"
 			+ " null, null, #{final_expect_date}, #{kanban_id})")
-	void createBoard(ProjectBoardDTO board);
-
+	boolean createBoard(ProjectBoardDTO board);
+	
+	// 유저별 보드와 kanban_name(board_status)불러오기
+	@Select("SELECT project_status, board_id, pb.project_id, board_subject, board_content, board_create_date, "
+			+ "start_user_email,  views, start_date, final_date, final_expect_date, pb.kanban_id "
+			+ "FROM "+  PROJECTBOARD + " pb, " + PROJECTSTATUS + " ps "
+			+ " where pb.kanban_id = ps.kanban_id and start_user_email=#{user_email}")
+	ArrayList<ProjectBoardJoinKanban> selectBoardJoinKanban(UserDTO user);
+	
 	// task 삭제
 	@Delete("DELETE FROM " + PROJECTBOARD + " WHERE board_id=#{board_id}")
-	void deleteBoard(ProjectBoardDTO board);	
+	boolean deleteBoard(ProjectBoardDTO board);	
 	
 	// 작업 내용 수정
 	@Update("UPDATE " + PROJECTBOARD
 			+ " board_subject=#{board_subject}, board_content=#{board_content}, start_user_email=#{start_user_email},"
 			+ "views=#{views}, start_date=#{start_date}, final_date=#{final_date}, final_expect_date=#{final_expect_date}, kanban_id=#{kanban_id}"
 			+ " WHERE board_id=#{board_id}")
-	void updateBoard(ProjectBoardDTO board);
+	boolean updateBoard(ProjectBoardDTO board);
 	
 	// 프로젝트 룰
 	// 프로젝트 룰 불러오기
@@ -93,11 +104,11 @@ public interface ProjectMapper extends DefaultDBInfo {
 	
 	// 프로젝트 룰 생성
 	@Insert("INSERT INTO " + PROJECTRULE + " VALUES (${project_id} ,#{rule}) ")
-	void createRule(ProjectRuleDTO rule);
+	boolean createRule(ProjectRuleDTO rule);
 	
 	// 프로젝트 룰 삭제
 	@Delete("DELETE FROM " + PROJECTRULE + " WHERE project_id=${project_id} and rule=#{rule}")
-	void deleteRule(ProjectRuleDTO rule);	
+	boolean deleteRule(ProjectRuleDTO rule);	
 	
 	// 댓글
 	// 태스크별 댓글 불러오기
@@ -106,11 +117,17 @@ public interface ProjectMapper extends DefaultDBInfo {
 	
 	// 댓글 입력
 	@Insert("INSERT INTO " + BOARDCOMMENT + " VALUES (null, #{board_id} ,#{user_email}, #{comment_content}) ")
-	void createComment(BoardCommentDTO comment); 
+	boolean createComment(BoardCommentDTO comment); 
 	
 	// 댓글 삭제
 	@Delete("DELETE FROM " + BOARDCOMMENT + " WHERE comment_id=#{comment_id}")
-	void deleteComment(BoardCommentDTO comment);
+	boolean deleteComment(BoardCommentDTO comment);
+	
+	// 그룹원인지 체크하기위한 메서드
+	@Select("SELECT project_id FROM " + PROJECTJOIN 
+			+ "where project_id = #{project_id} and user_email= #{user_email} "
+			+ "and join_status in('admin', 'member')" )
+	ProjectJoinDTO selctGroupCheck(ProjectDTO project, UserDTO user);
 	
 	// 그룹원 목록 불러오기
 	@Select("SELECT * FROM " + PROJECTJOIN + " where project_id=${project_id}") 
@@ -118,15 +135,15 @@ public interface ProjectMapper extends DefaultDBInfo {
 	
 	// 그룹원 추가하기
 	@Insert("INSERT INTO " + PROJECTJOIN + " VALUES (${project_id}, #{user_email}, #{role}, #{join_status} )")
-	void insertGroup(ProjectJoinDTO join);
+	boolean insertGroup(ProjectJoinDTO join);
 	
 	// 그룹원 삭제
 	@Delete("DELETE FROM " + PROJECTJOIN + " WHERE project_id=${project_id} and user_email=#{user_email}")
-	void deleteGroup(ProjectJoinDTO join);
+	boolean deleteGroup(ProjectJoinDTO join);
 	
 	// 그룹원 요청 처리
 	@Update("UPDATE " + PROJECTJOIN + "join_status=#{join_status} WHERE user_email=#{user_email}")		
-	void updateGroup(ProjectJoinDTO join);
+	boolean updateGroup(ProjectJoinDTO join);
 	
 	// 회의
 	// 호의 목록 불러오기
@@ -136,11 +153,11 @@ public interface ProjectMapper extends DefaultDBInfo {
 	// 회의 일정 생성
 	@Insert("INSERT INTO " + MEETING
 			+ " VALUES (null, #{meeting_title}, #{meeting_start_date}, #{meeting_end_date}, ${project_id})")
-	void createMeeting(MeetingDTO meeting);
+	boolean createMeeting(MeetingDTO meeting);
 	
 	// 회의 정보 삭제
 	@Delete("DELETE FROM " + MEETING + " WHERE meeting_id=#{meeting_id}")
-	void deleteMeeting(MeetingDTO meeting);
+	boolean deleteMeeting(MeetingDTO meeting);
 	
 	// 회의 주제 불러오기
 	@Select("SELECT * FROM " + MEETINGLOG + " WHERE meeting_id=#{meeting_id}")
@@ -148,11 +165,11 @@ public interface ProjectMapper extends DefaultDBInfo {
 	
 	// 회의 주제 추가
 	@Insert("INSERT INTO " + MEETINGLOG + " VALUES (null, #{meeting_id}, #{meeting_log_subject}, #{user_email})")
-	void insertMeetingLog(MeetingLogDTO meetingLog);
+	boolean insertMeetingLog(MeetingLogDTO meetingLog);
 	
 	// 회의 주제 삭제
 	@Delete("DELETE FROM " + MEETINGLOG + " WHERE meeting_log_id=#{meeting_log_id}")
-	void deleteMeetingLog(MeetingLogDTO meetingLog);
+	boolean deleteMeetingLog(MeetingLogDTO meetingLog);
 	
 	// 주제에대한 의견 불러오기
 	@Select("SELECT * FROM " + MEETINGLOGCHAT + " WHERE meeting_log_id=#{meeting_log_id} ")
@@ -160,11 +177,11 @@ public interface ProjectMapper extends DefaultDBInfo {
 	
 	// 주제에대한 의견 생성
 	@Insert("INSERT INTO " + MEETINGLOGCHAT + " VALUES (null, #{meeting_log_id}, #{meeting_log_opinion}, #{user_email})")
-	ArrayList<MeetingLogChatDTO> insertMeetingLogChat(MeetingLogChatDTO meetinglogDTO);
+	boolean insertMeetingLogChat(MeetingLogChatDTO meetinglogDTO);
 	
 	// 회의 주제 삭제
 	@Delete("DELETE FROM " + MEETINGLOGCHAT + " WHERE meeting_log_chat_id=#{meeting_log_chat_id}")
-	void deleteMeetingChat(MeetingLogChatDTO meetingLog);
+	boolean deleteMeetingChat(MeetingLogChatDTO meetingLog);
 		
 
 }

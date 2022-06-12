27b0b80a77e4435ff00f3720c22462293768425a
JaeRyeong Jung <73861946+Rwhisper@users.noreply.github.com>
2022-06-12 10:05:15 +0900

    update modal.js , update mypage, nav_sidbar, project, settings,

--- src/main/resources/static/assets/js/modal.js
+++ src/main/resources/static/assets/js/modal.js
@@ -14,3 +14,32 @@ $('#modal_edit_task').on('shown.bs.modal', function (event) {
     $('#start_user_email').html(e.data('start_user_email')) ;
     
 });
+
+$('#modal_edit_group').on('shown.bs.modal', function (event) { 
+  var e = $(event.relatedTarget);    
+  $('#edit_user_con').html(e.data('user_email')) ;    $('#edit_user').html(e.data('user_email')) ;
+    $('#role').val(e.data('role')) ;
+    
+});
+
+$('#modal_del_group').on('shown.bs.modal', function (event) { 
+  var e = $(event.relatedTarget);    
+  $('#del_user').val(e.data('user_email')) ;
+    $('#del_user_con').html(e.data('user_email')) ;
+    
+});
+
+$('#modal_edit_status').on('shown.bs.modal', function (event) { 
+  var e = $(event.relatedTarget);    
+  $('#board_id').val(e.data('boardid')) ;
+    $('#project_id').val(e.data('projectid')) ;
+    
+});
+
+
+
+// $('#edit_task').on('click', function (event) { 
+//     event.preventDefault();
+//     $('#modal_edit_task').modal('show').find('.modal-body').load($(this).attr('href'));   
+    
+// });

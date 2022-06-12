5e24897174d0ae65f7a972880fa743f9ac5f8a38
JaeRyeong Jung <73861946+Rwhisper@users.noreply.github.com>
2022-06-12 10:07:33 +0900

    add project_name parameter in ProjectJoinDTO

--- src/main/java/com/pmis/model/ProjectJoinDTO.java
+++ src/main/java/com/pmis/model/ProjectJoinDTO.java
@@ -7,5 +7,8 @@ public class ProjectJoinDTO {
 	private int project_id;
 	private String user_email;
 	private String role;
-	private String join_status;
+	private String join_status;	
+	
+	// table에는 없는 값, default=null로 받고 필요할때만 join 해서 사용할 값
+	private String project_name;
 }
